e0c514a7fbdb9153f128edab1a66f3f7940eba23
Zachary King <zak@delfina.com>
2025-10-20 17:58:37 -0700

    inngest

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+import { inngest } from "../../../inngest/client"; // Import our client
+
+// Opt out of caching; every request should send a new event
+export const dynamic = "force-dynamic";
+
+// Create a simple async Next.js API route handler
+export async function GET() {
+  // Send your event payload to Inngest
+  await inngest.send({
+    name: "test/hello.world",
+    data: {
+      email: "testUser@example.com",
+    },
+  });
+
+  return NextResponse.json({ message: "Event sent!" });
+}
